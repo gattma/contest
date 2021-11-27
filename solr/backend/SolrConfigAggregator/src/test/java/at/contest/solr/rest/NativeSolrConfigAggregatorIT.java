e5faa3ba3070ce87637c1748328013e58fcc719f
mgattringer <marko.gattringer@gepardec.com>
2021-11-27 11:04:08 +0100

    removed tests

--- solr/backend/SolrConfigAggregator/src/test/java/at/contest/solr/rest/NativeSolrConfigAggregatorIT.java
+++
@@ -1,9 +0,0 @@
-package at.contest.solr.rest;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeSolrConfigAggregatorIT extends SolrConfigAggregatorTest {
-
-    // Execute the same tests but in native mode.
-}
