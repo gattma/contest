329fe3e05383dfdde54e0691fe49907b1063ae3e
mgattringer <marko.gattringer@gepardec.com>
2021-11-27 09:09:33 +0100

    added liveness and readiness checks

---
+++ solr/backend/SolrSearchService/src/main/java/at/contest/solr/health/SimpleReadinessCheck.java
@@ -0,0 +1,17 @@
+package at.contest.solr.health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+@Readiness
+public class SimpleReadinessCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("App is ready").up().build();
+    }
+}
